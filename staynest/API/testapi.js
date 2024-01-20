7a56c463e84317c9385efcc2e5b573093ed7e9d1
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-01-21 01:45:21 +0600

    Remove ignored files

--- staynest/API/testapi.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default async function Testapi() {
-    const response = await fetch("http://localhost:8000/api/HomeData");
-    const data = await response.json();
-    // console.log(data);
-    return data;
-
-}
