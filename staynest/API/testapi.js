5e11c494ba19a7a0cd195d82b4535a32247b0475
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-01-20 22:35:20 +0600

    ready to install

--- staynest/API/testapi.js
+++ staynest/API/testapi.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function Testapi() {
-    const response = await fetch("http://localhost:8000/HomeData");
+    const response = await fetch("http://localhost:8000/api/HomeData");
     const data = await response.json();
     // console.log(data);
     return data;
