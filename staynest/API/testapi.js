f1b65e9a5abeece8685bc4d8d99f2c03ef34f892
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-01-21 01:49:12 +0600

    gitgnore added and tested

---
+++ staynest/API/testapi.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default async function Testapi() {
+    const response = await fetch("http://localhost:8000/api/HomeData");
+    const data = await response.json();
+    // console.log(data);
+    
+    return data;
+
+}
