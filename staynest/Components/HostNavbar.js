2805111beb08a3dfc2a46fa4661d9911ec052351
Tehjeeb <syedtehjeeb2001@gmail.com>
2024-01-27 01:38:32 +0600

    nav bar coloured and added guest/host button

--- staynest/Components/HostNavbar.js
+++ staynest/Components/HostNavbar.js
@@ -1,22 +1,52 @@
 // components/Navbar.js
+'use client';
 import Link from 'next/link';
 import React from 'react';
-import '@/Components/style/HostNavbar.css';
+import Image from 'next/image'
+import Logo from '@/StaticImage/Meta_Inc._logo.jpg'
 
-const Navbar = () => (
-  <nav className="navbar">
-    <Link href="/">
-      <div className="logo">Your Logo Here</div>
-    </Link>
-    <Link href="/host">
-        {/* no anchor tag in nextjs 14 */}
-        <div className="account-image">StayNest</div>
+export default function Navbar() { 
 
-    </Link>
-    <Link href="/account">
-        <div className="logo">Your Account</div>
-    </Link>
+  const [userMode, setUserMode] = React.useState(false);
+  function hostMode() {
+    setUserMode(false);
+  }
+  function guestMode() {
+    setUserMode(true);
+  }
+
+  return (
+  <nav className="bg-yellow-400 p-1">
+    <dif className="mx-auto flex justify-between items-center">
+      <Link href="/">
+        <div ><Image
+      src={Logo}
+      width={65}
+      alt="Picture of the author"
+    /></div>
+      </Link>
+      <Link href="/host">
+          <div className="font-bold text-2xl">StayNest</div>
+      </Link>
+
+      <div className="flex">
+        <div className="flex">
+          <button class={`${userMode ? 'bg-black'  : 'bg-blue-700'} text-white font-bold py-2 px-4 rounded `} onClick={hostMode}>
+              host
+          </button>
+          <button class={`${userMode ? 'bg-blue-700' : 'bg-black'} text-white font-bold py-2 px-4 rounded`} onClick={guestMode}>
+              guest
+          </button>
+        </div>
+        <Link href="/account">
+            <div> 
+              <Image src={Logo} width={65} alt="Picture of the author"/> 
+            </div>
+        </Link>
+      </div>
+      
+      </dif>
+
   </nav>
 );
-
-export default Navbar;
+}
