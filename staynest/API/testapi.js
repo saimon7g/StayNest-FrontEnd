8c50476a92699c6e7d84a6de856760b914b12ab1
Rakibul Hasan Rafi <92964753+rafi524@users.noreply.github.com>
2024-01-28 00:38:02 +0600

    port changed

--- staynest/API/testapi.js
+++ staynest/API/testapi.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function Testapi() {
-    const response = await fetch("http://localhost:8000/api/HomeData");
+    const response = await fetch("http://127.0.0.1:8000/api/HomeData");
     const data = await response.json();
     // console.log(data);
     
