a9b655807600a923a0915eef74f3788b7b017181
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2024-02-06 00:40:55 +0600

    axios addeed to all api

--- staynest/API/Registration.js
+++ staynest/API/Registration.js
@@ -14,54 +14,38 @@ export async function Step1GET() {
 export async function Step1Post(data) {
     console.log(data);
     try {
-        const response = await fetch("http://127.0.0.1:8000/host/api/property_registration/step1/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-        const result = await response.json();
-        console.log(result);
-        return result;
+        const response = await axios.post("http://127.0.0.1:8000/host/api/property_registration/step1/", data);
+        console.log(response.data);
+        return response.data;
     }
     catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
-
 
 export async function Step2PUT(data) {
     console.log("step2put");
     console.log(data);
     console.log("step2put                    11111111 ");
     try {
-        const response = await fetch("http://127.0.0.1:8000/host/api/property_registration/step2/6/", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data),
-        });
-        const result = await response.json();
-        console.log(result);
-        return result;
+        const response = await axios.put("http://127.0.0.1:8000/host/api/property_registration/step2/3/", data);
+        console.log(response.data);
+        return response.data;
     }
     catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
 export async function Step2GET() {
     console.log("step2get");
     try {
-        const response = await fetch("http://127.0.0.1:8000/host/api/property_registration/step2/3/");
-        const result = await response.json();
-        console.log(result);
-        return result;
+        const response = await axios.get("http://127.0.0.1:8000/host/api/property_registration/step2/3/");
+        console.log(response.data);
+        return response.data;
     }
     catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
@@ -83,18 +67,11 @@ export async function Step5PUT(data) {
     console.log(data);
     console.log("step5put                    11111111 ");
     try {
-        const response = await fetch("http://127.0.0.1:8000/host/api/property_registration/step5/3/", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data),
-        });
-        const result = await response.json();
-        console.log(result);
-        return result;
+        const response = await axios.put("http://127.0.0.1:8000/host/api/property_registration/step5/3/", data);
+        console.log(response.data);
+        return response.data;
     }
     catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
