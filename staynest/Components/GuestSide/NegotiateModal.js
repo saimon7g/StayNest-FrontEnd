bc3714c15a2ad268265ce94c693dcb245d783216
Rakibul Hasan Rafi <92964753+rafi524@users.noreply.github.com>
2024-02-24 09:58:43 +0600

    Modal floating problem solved

--- staynest/Components/GuestSide/NegotiateModal.js
+++ staynest/Components/GuestSide/NegotiateModal.js
@@ -5,22 +5,22 @@ import { Modal , Button} from 'flowbite-react';
 
 
 
-
-
-
 export function NegotiationModal( {data, openModal, setOpenModal} ) {
+
+    
     return (
-        <Modal open={openModal} onClose={() => setOpenModal(false)}>
+         <Modal show={openModal} size="xl" popup onClose={()=>setOpenModal(false)}>
+        
            <Modal.Header className="bg-primary">
             <h2>Request for Negotiation</h2>
             </Modal.Header>
-            <Modal.Body className="bg-black">
+            <Modal.Body >
                 <p>Request for negotiation has been sent to the host. You will be notified once the host responds to your request.</p>
                 <p>Request for negotiation has been sent to the host. You will be notified once the host responds to your request.</p>
                 
             </Modal.Body>
             <Modal.Footer className="bg-white">
-                <Button className="bg-white"
+                <Button 
                 onClick={ () => setOpenModal(false) } >
                     Close
                     </Button>
